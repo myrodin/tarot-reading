71097068305ab523f6fde1056856e95d00ee05af
myrodin <myrodin@naver.com>
2025-11-04 00:07:06 +0900

    Fix: Add basename to React Router for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Reading from './pages/Reading';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/tarot-reading">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/reading/:type" element={<Reading />} />
